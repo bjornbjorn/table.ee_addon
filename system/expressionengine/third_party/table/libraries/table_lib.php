2bfb25f9e5cd022fde403df207ac7cfa5ba687a1
Bjørn Børresen <bjorn@lastfriday.no>
2014-05-30 13:11:58 +0200

    feat(tag): added support for col_limit / col_offset
    {table_field_name col_limit="1" col_offset="1"} can now be used to specify column limit/offset

--- system/expressionengine/third_party/table/libraries/table_lib.php
+++ system/expressionengine/third_party/table/libraries/table_lib.php
@@ -77,6 +77,8 @@ class Table_lib {
     {
         $row_limit = isset($params['row_limit']) ? $params['row_limit'] : FALSE;
         $row_offset = isset($params['row_offset']) ? $params['row_offset'] : FALSE;
+        $col_limit = isset($params['col_limit']) ? $params['col_limit'] : FALSE;
+        $col_offset = isset($params['col_offset']) ? ($params['col_offset'] + 1) : 1;
 
         $table_name = Table_ft::TABLE_PREFIX.$field_name;
         $num_cols = 0;
@@ -98,13 +100,15 @@ class Table_lib {
                 $row_num++;
                 $row_type = $row['row_type'];
                 $col = array();
-                $i=1;
-                while(isset($row['col_'.$i])) {
+                $i=$col_offset;
+                $col_counter = 0;
+                while(isset($row['col_'.$i]) && ($col_limit == FALSE || $col_counter < $col_limit)) {
+                    $col_counter++;
                     $raw_content = $row['col_'.$i];
                     $content = $this->parse_cell_content($row_type, $raw_content);
                     $col[] =
                         array(
-                            Table_ft::TAG_PREFIX.'col:num' => $i,
+                            Table_ft::TAG_PREFIX.'col:num' => $col_counter,
                             Table_ft::TAG_PREFIX.'col:content' => $content,
                             Table_ft::TAG_PREFIX.'col:content_raw' => $raw_content,
                             Table_ft::TAG_PREFIX.'col:content:num_words' => str_word_count($raw_content),   // @todo: raw_content = text content, for others this won't make sense?
@@ -114,7 +118,7 @@ class Table_lib {
                     $i++;
                 }
 
-                $num_cols = $i-1;
+                $num_cols = $col_counter;
 
                 $rows[] = array(
                     Table_ft::TAG_PREFIX.'row:num' => $row_num,
@@ -143,14 +147,6 @@ class Table_lib {
 
                 $mobile_tables[] = array( Table_ft::TAG_PREFIX.'mobile:rows' => $mobile_rows );
             }
-
-
-
-
-            //print_r($rows);
-
-         //   print_r($mobile_tables);
-         //   die();
 
             $vars = array(
                 Table_ft::TAG_PREFIX.'total_rows' => count($rows),
